c3fd4e9675bd22167ca306cca3193778e15c5d0b
chadonet-65 <chadonetthiezar@gmail.com>
2023-01-13 22:03:12 +0000

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     key = None
     if a_dictionary:
-        for idx, item in dictionary.items():
+        for idx, item in a_dictionary.items():
             if item > a_dictionary.get(key, 0):
                 key = idx
-    return (key)
+    return (keyi)
