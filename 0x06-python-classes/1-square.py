83e7dc009bcd8699a9cbcfda303d06818028e3bf
chadonet-65 <chadonetthiezar@gmail.com>
2023-01-24 13:53:13 +0000

    added new tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Define a class Square"""
 
+
 class Square:
-    """Initialisation a new Square.
-    Args:
-        size (int): The size of new square.
-    """
+    """Description a Square"""
+
     def __init__(self, size):
+        """Initialisation a new Square.
+        Args:
+            size (int): The size of new square.
+        """
         self.__size = size
