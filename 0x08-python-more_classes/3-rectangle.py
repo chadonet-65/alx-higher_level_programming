926e0cfee3a4d35cc48214897d8f1c5ab1e2e578
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-02-01 17:03:18 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,40 +3,33 @@
 
 class Rectangle:
     """At this stage only creates private instance attribute by talking in two arguments.
-
     Args:
        width (int): horizontal dimension a rectangle
        height (int): vertical dimension a rectangle
-
     """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-         """__width getter
-
+        """__width getter
          Returns:
             __width (int): horizontal dimension a rectangle
-
          """
-         return self.__width
+        return self.__width
 
-     @width.setter
-     def width(self, value):
-         """__width setter
-
+    @width.setter
+    def width(self, value):
+        """__width setter
          Args:
             value (int): horizontal dimension a rectangle
-
         Attribute:
             __width (int): horizontal dimension a rectangle
-
         Raises:
              TypeError: If `value` is not an int
              ValueError: If `value` is less than 0.
-
         """
         if type(value) is not int:
             raise TypeError('width must be an integer')
@@ -53,11 +46,11 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('heigt must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width is 0 or self.__height is 0:
@@ -69,7 +62,7 @@ class Rectangle:
         string = ""
         for row in range(self.__height):
             for col in range(self.__width):
-                string +='#'
+                string += '#'
             if self.__width != 0 and row < (self.__height - 1):
                 string += '\n'
         return string
