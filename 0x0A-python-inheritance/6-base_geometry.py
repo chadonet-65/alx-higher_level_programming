5c2a8843d09ba7004e6f28ca0d96e69c0cb694eb
chadonet-65 <chadonetthiezar@gmail.com>
2023-02-23 09:58:23 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""Create a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represent a base geometry."""
+
+    def area(self):
+        """Implementation a function area."""
+        raise Exception("area() is not implemented")
