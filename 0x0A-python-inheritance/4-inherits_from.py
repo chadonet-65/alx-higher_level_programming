36a5119e1a08de80125b1398650101151c46cba1
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-02-23 10:17:19 +0100

    Update 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 4-inherits_from(.py
+"""Defines a class function"""
+
+
+def inherits_from((obj, a_class):
+    """Check if an object is exactly an instance of given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Rteurns:
+        If obj is exactly an instance of a_class
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
