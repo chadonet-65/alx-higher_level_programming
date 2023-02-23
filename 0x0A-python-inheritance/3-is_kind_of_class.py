371915c097a65a5176fc5f9303da3370009bd38c
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-02-23 10:08:43 +0100

    Update 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Defines a class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is exactly an instance of given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Rteurns:
+        If obj is exactly an instance of a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
