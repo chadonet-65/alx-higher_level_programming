d29dcee2ed078a6d1206cc9285b87a6b2ae8f443
chadonet-65 <chadonetthiezar@gmail.com>
2023-03-04 09:52:40 +0000

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""
+  3-to_json_string
+  function to return json object
+"""
+
+
+def to_json_string(my_obj):
+    """return json object"""
+    return json.dumps(my_obj)
