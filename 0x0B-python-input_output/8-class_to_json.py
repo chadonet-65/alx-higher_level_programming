15511853a92e212b0199b7d8122d892eea2b013a
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-03-05 20:53:12 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+  8-class_to_json.py
+  function to return json object
+"""
+
+
+def class_to_json(obj):
+    dic = {}
+    if hasattr(obj, '__dict__'):
+        dic = json.load(obj)
+    return dic
