5ca77eedd142284c2cec2457f25c42aa3fa62ad8
chadonet-65 <chadonetthiezar@gmail.com>
2023-03-14 04:56:40 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Gropu by
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score; 
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- List all element 
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
