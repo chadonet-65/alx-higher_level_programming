555377690693bf1c42f2919a83c096624996bc8d
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-03-16 22:32:26 +0100

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,6 @@
--- List all cities of california
-SELECT cities.id, cities.name FROM cities, state  WHERE state.id = cities.state_id AND cities.name = 'California' ORDER BY cities.id ASC;
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+    AND states.name = 'California'
+ORDER BY cities.id ASC;
